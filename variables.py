df09537f10eccc8cff8381e07a4c961d9ce28471
geethika <geethikas7983@gmail.com>
2026-02-01 11:53:02 -0500

    python variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+#Define variables of different data types
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, World!"
+boolean_var = True
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (10, 20, 30)
+dict_var = {"name": "Alice", "age": 30}
+set_var = {1, 2, 3, 4, 5}
+
+# Print and manipulate  variables
+print("Integer Variable:", integer_var)
+print("Float Variable:", float_var)
+print("String Variable:", string_var + " How are you?")
+print("Boolean Variable:", boolean_var)
+print("List Variable:", list_var)
+print("Tuple Variable:", tuple_var)
+print("Dictionary Variable:", dict_var["name"])
+print("Set Variable:", set_var) 
+
+
